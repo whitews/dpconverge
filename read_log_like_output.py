9ec56e8b6b2d890fb83d01625e48091c9eec619a
Scott White <whitews@gmail.com>
2016-03-11 09:58:58 -0500

    added helper scrip t to read & plot log likelihood output

---
+++ read_log_like_output.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot
+
+df = pd.read_pickle('log_like_data.pkl')
+
+component_list = df.comp.unique()
+seeds = df.seed.unique()
+
+cmap = pyplot.cm.get_cmap('jet')
+colors = [cmap(i) for i in np.linspace(0, 1, len(component_list))]
+alphas = np.linspace(1, 0.4, len(component_list))
+
+fig = pyplot.figure(figsize=(8, 8))
+
+for i, comp_count in enumerate(component_list):
+    color = colors[i]
+    for seed in seeds:
+
+        df[(df.comp == comp_count) & (df.seed == seed)].plot(
+            x='iter',
+            y='likelihood',
+            c=color,
+            linewidth=1,
+            alpha=alphas[i]
+        )
+
+pyplot.show()
+
+pass
