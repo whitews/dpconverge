61e08085fbe00cad8478c88d3f5afba6880bec4f
Scott White <whitews@gmail.com>
2016-03-16 17:06:11 -0400

    update read log like output script to specify a starting iteration to plot

--- read_log_like_output.py
+++ read_log_like_output.py
@@ -4,24 +4,26 @@ from matplotlib import pyplot
 
 df = pd.read_pickle('log_like_data.pkl')
 
+start_iter = 200
+
 component_list = df.comp.unique()
 seeds = df.seed.unique()
 
 cmap = pyplot.cm.get_cmap('jet')
-colors = [cmap(i) for i in np.linspace(0, 1, len(component_list))]
+colors = [cmap(i) for i in np.linspace(0, 1, len(seeds))]
 alphas = np.linspace(1, 0.4, len(component_list))
 
 fig = pyplot.figure(figsize=(8, 8))
 
 for i, comp_count in enumerate(component_list):
-    for seed in seeds:
+    for j, seed in enumerate(seeds):
 
-        df[(df.comp == comp_count) & (df.seed == seed)].plot(
+        df[(df.comp == comp_count) & (df.seed == seed) & (df.iter >= start_iter)].plot(
             x='iter',
             y='likelihood',
-            c=colors[i],
-            linewidth=1,
-            alpha=alphas[i]
+            c=colors[j],
+            linewidth=1.0,
+            alpha=0.8
         )
 
 pyplot.show()
