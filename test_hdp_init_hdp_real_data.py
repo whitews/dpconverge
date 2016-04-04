f098ee62ca5d72cd50f12756872d3c3253e0c410
Scott White <whitews@gmail.com>
2016-04-04 17:02:11 -0400

    update hdp init script, mostly working now

--- test_hdp_init_hdp_real_data.py
+++ test_hdp_init_hdp_real_data.py
@@ -63,21 +63,28 @@ for fcs_file in fcs_files:
 
     dc.add_data_set(ds)
 
-component_count = 8
+iteration_count = 1000
+component_count = 16
 seeds = range(1, 9)
 converged = False
 results = []  # will be a list of dicts to convert to a DataFrame
 
 while not converged:
-    print component_count
+    print "Comp count:", component_count
+
+    last_results_count = len(results)
 
     for seed in seeds:
+        print "Seed:", seed
+
         dc.reset_results()
 
-        dc.cluster(
+        tmp_converged = False
+
+        hdp_mixture = dc.cluster(
             component_count=component_count,
             burn_in=1000,
-            iteration_count=1000,
+            iteration_count=iteration_count,
             random_seed=seed
         )
 
@@ -87,14 +94,92 @@ while not converged:
         valid_components = dc.data_sets[0].get_valid_components(
             ignore_weight=True
         )
+        valid_comp_count = len(valid_components)
 
-        print "Recommended component count: ", len(valid_components)
+        print "Stable component count: ", valid_comp_count
 
-        for r_ds in dc.data_sets:
-            for i in range(component_count):
-                if i not in valid_components:
-                    print "Possible invalid Component"
-                    r_ds.plot_iteration_traces(i)
+        if valid_comp_count < component_count:
+            progress = True
+            while progress:
+                # build initial conditions from last iteration
+                pis = []
+                for i, ds in enumerate(dc.data_sets):
+                    last_iter = hdp_mixture[i].get_iteration(
+                        iteration_count - 1
+                    )
+                    pis.append(last_iter.pis)
 
-            r_ds.plot_log_likelihood_trace()
-            # r_ds.plot_animated_trace()
+                initial_conditions = {
+                    'pis': np.array(pis),
+                    'mus': last_iter.mus,
+                    'sigmas': last_iter.sigmas
+                }
+
+                # reset results
+                dc.reset_results()
+
+                # continue cluster for another round of iterations
+                hdp_mixture = dc.cluster(
+                    component_count=component_count,
+                    burn_in=0,  # we want to start off where it left off
+                    iteration_count=iteration_count,
+                    random_seed=seed,
+                    initial_conditions=initial_conditions
+                )
+
+                new_valid_comps = dc.data_sets[0].get_valid_components(
+                    ignore_weight=True
+                )
+
+                print "Stable component count: ", len(new_valid_comps)
+
+                if len(new_valid_comps) > valid_comp_count:
+                    # check if it's equal to comp count
+                    if len(new_valid_comps) == component_count:
+                        # chain stabilized, we're good to save these results
+                        tmp_converged = True
+                        break
+                    else:
+                        valid_comp_count = len(new_valid_comps)
+                else:
+                    progress = False
+
+        else:
+            # valid comps equal comp count
+            tmp_converged = True
+
+        if tmp_converged:  # valid comps equal comp count
+            # average results and get the sum of log likelihoods across
+            # data sets
+            hdp_mixture_avg = hdp_mixture.average()
+
+            log_likelihoods = []
+
+            for i, ds in enumerate(dc.data_sets):
+                data = np.vstack(ds.blobs.values())
+
+                dp_mixture = hdp_mixture_avg[i]
+
+                log_likelihoods.append(
+                    dp_mixture.log_likelihood(data)
+                )
+
+            # save results for this run
+            results.append(
+                {
+                    'seed': seed,
+                    'comp': component_count,
+                    'log_like': sum(log_likelihoods),
+                    'results': hdp_mixture_avg
+                }
+            )
+
+    if len(results) > last_results_count:
+        # still generating stable chains, bump the component count and
+        # keep going
+        component_count += 1
+    else:
+        converged = True
+
+# now, what to do with all these results?
+print len(results)
